119d4e46ace72032a07630f6da06bd205e2d6c6d
Noe De Santo <noe.desanto@epfl.ch>
2021-03-31 11:17:47 +0200

    Change direct left recursion elimination so that it returns a recursive syntax

--- src/main/scala/scallion/factorization/LeftRecursion.scala
+++ src/main/scala/scallion/factorization/LeftRecursion.scala
@@ -4,13 +4,13 @@ package factorization
 trait LeftRecursion extends LeftFactorization with Substitution { self: Syntaxes with SyntaxesProperties =>
   import Syntax._
 
-  def eliminateDirectLeftRecursion[A](syntax: Recursive[A]): Syntax[A] = {
+  def eliminateDirectLeftRecursion[A](syntax: Recursive[A]): Recursive[A] = {
     val (recPart, simplePart) = leftFactorOut(syntax, syntax.inner)
-    val factorized = (simplePart ~ many(recPart)).map{
+    val factorized = recursive( (simplePart ~ many(recPart)).map{
       case scallion.~(base, follows) => follows.foldLeft(base)((current, follow) => follow(current))
-    }
+    })
 
-    eliminate(factorized, syntax, factorized)
+    eliminate(factorized, syntax, factorized).asInstanceOf[Recursive[A]]
   }
 
 }
