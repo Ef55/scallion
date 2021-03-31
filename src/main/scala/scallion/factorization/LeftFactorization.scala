d2ca6476a77a5dba7ea5e439b1f73c0a9eca1205
Noe De Santo <noe.desanto@epfl.ch>
2021-03-31 10:35:44 +0200

    Refactor LeftFactorization internally for future use

--- src/main/scala/scallion/factorization/LeftFactorization.scala
+++ src/main/scala/scallion/factorization/LeftFactorization.scala
@@ -9,16 +9,111 @@ trait LeftFactorization { self: Syntaxes with SyntaxesProperties =>
 
   import Syntax._
 
-  /** Left factorizes a single terminal in the syntax.
-    * 
-    * @param leftFactor Terminal to left factor out.
-    * @param s Syntax on which to apply the factorization.
-    * @return An equivalent syntax, with the terminal factorized.
-    *
-    * @group factorization
-    */
-  def leftFactorize[A](leftFactor: Kind, s: Syntax[A]): Syntax[A] = {
-    leftFactorize(Elem(leftFactor), s)
+  private case class Factorization[A, L](
+    factorized: Syntax[L => A],
+    alternative: Syntax[A]
+  ){
+    def |(that: Factorization[A, L]): Factorization[A, L] = {
+      Factorization(
+        this.factorized | that.factorized,
+        this.alternative | that.alternative
+      )
+    }
+
+    def ~[F](follow: Syntax[F]): Factorization[A ~ F, L] = {
+      Factorization(
+        (this.factorized ~ follow).map(
+            _ match { case fl ~ r => (l: L) => scallion.~(fl(l), r)},
+            _ match { case _ => throw new IllegalArgumentException("Reverse transformation not yet implemented") }
+        ),
+        this.alternative ~ follow
+      )
+    }
+
+    def prepend[P](prefix: Syntax[P]): Factorization[P ~ A, L] = {
+      Factorization(
+        (prefix ~ this.factorized).map(
+          _ match { case l ~ fr => (r: L) => scallion.~(l, fr(r)) },
+          _ match { case _ => throw new IllegalArgumentException("Reverse transformation not yet implemented") }
+        ),
+        prefix ~ this.alternative
+      )
+    }
+
+    def map[B](fun: A => B, inv: B => Seq[A] = (b: B) => Seq()): Factorization[B, L] = {
+      Factorization(
+        this.factorized.map(
+          factorized => (l: L)  => fun(factorized(l)),
+          factorized => throw new IllegalArgumentException("Reverse transformation not yet implemented")
+        ),
+        this.alternative.map(fun, inv)
+      )
+    }
+
+    def asRecursive: Factorization[A, L] = {
+      Factorization(
+        recursive(this.factorized),
+        recursive(this.alternative)
+      )
+    }
+
+    def complete(leftFactor: Syntax[L]): Syntax[A] = {
+      (leftFactor ~ this.factorized).map(
+        _ match { case l ~ f => f(l) },
+        (_: A) => throw new IllegalArgumentException("Reverse transformation not yet implemented")
+      ) | this.alternative
+    }
+  }
+
+  private object Factorization {
+    def success[L]: Factorization[L, L] = {
+      Factorization(
+        Success(()).map( 
+          (_: Unit) => (l: L) => {
+            l
+          }
+        ),
+        failure
+      )
+    }
+
+    def fail[A, L](s: Syntax[A]): Factorization[A, L] = {
+      Factorization(
+        failure,
+        s
+      )
+    }
+  }
+
+  private def internal[A, L](leftFactor: Syntax[L], s: Syntax[A]): Factorization[A, L] = {
+
+    def iter[A](s: Syntax[A]): Factorization[A, L] = {
+      s match {
+        case _ if s == leftFactor       => Factorization.success.asInstanceOf[Factorization[A, L]]
+        case e: Elem                    => Factorization.fail(e)
+        case Sequence(l, r)             => {
+          val p = getProperties(l)
+          if(p.isNull){
+            iter(r).prepend(l)
+          }
+          else{
+            // If the syntax is nullable, it might be possible
+            // that we should factorize both parts; we don't do this
+            // in order to stay simple (and because this case implies
+            // a first-follow conflict)
+            iter(l) ~ r
+          }
+        }
+        case Disjunction(l, r)          => iter(l) | iter(r)
+        case Transform(fun, inv, inner) => iter(inner).map(fun, inv)
+        case Marked(mark, inner)        => iter(inner) // mark ignored
+        case s: Success[_]              => Factorization.fail(s)
+        case Failure()                  => Factorization(failure, failure)
+        case Recursive(_, inner)        => iter(inner).asRecursive
+      }
+    }
+
+    iter(s)
   }
 
   /** Left factorizes a syntax in the syntax.
@@ -34,117 +129,20 @@ trait LeftFactorization { self: Syntaxes with SyntaxesProperties =>
     * @group factorization
     */
   def leftFactorize[A, L](leftFactor: Syntax[L], s: Syntax[A]): Syntax[A] = {
-
-    case class Factorization[A](
-      factorized: Syntax[L => A],
-      alternative: Syntax[A]
-    ){
-      def |(that: Factorization[A]): Factorization[A] = {
-        Factorization(
-          this.factorized | that.factorized,
-          this.alternative | that.alternative
-        )
-      }
-
-      def ~[F](follow: Syntax[F]): Factorization[A ~ F] = {
-        Factorization(
-          (this.factorized ~ follow).map(
-              _ match { case fl ~ r => (l: L) => scallion.~(fl(l), r)},
-              _ match { case _ => throw new IllegalArgumentException("Reverse transformation not yet implemented") }
-          ),
-          this.alternative ~ follow
-        )
-      }
-
-      def prepend[P](prefix: Syntax[P]): Factorization[P ~ A] = {
-        Factorization(
-          (prefix ~ this.factorized).map(
-            _ match { case l ~ fr => (r: L) => scallion.~(l, fr(r)) },
-            _ match { case _ => throw new IllegalArgumentException("Reverse transformation not yet implemented") }
-          ),
-          prefix ~ this.alternative
-        )
-      }
-
-      def map[B](fun: A => B, inv: B => Seq[A] = (b: B) => Seq()): Factorization[B] = {
-        Factorization(
-          this.factorized.map(
-            factorized => (l: L)  => fun(factorized(l)),
-            factorized => throw new IllegalArgumentException("Reverse transformation not yet implemented")
-          ),
-          this.alternative.map(fun, inv)
-        )
-      }
-
-      def mark(mark: Mark): Factorization[A] = {
-        Factorization(
-          this.factorized.mark(mark),
-          this.alternative.mark(mark)
-        )
-      }
-
-      def asRecursive: Factorization[A] = {
-        Factorization(
-          recursive(this.factorized),
-          recursive(this.alternative)
-        )
-      }
-
-      def complete: Syntax[A] = {
-        (leftFactor ~ this.factorized).map(
-          _ match { case l ~ f => f(l) },
-          (_: A) => throw new IllegalArgumentException("Reverse transformation not yet implemented")
-        ) | this.alternative
-      }
-    }
-
-    object Factorization {
-      def success: Factorization[L] = {
-        Factorization(
-          Success(()).map( 
-            (_: Unit) => (l: L) => {
-              l
-            }
-          ),
-          failure
-        )
-      }
-
-      def fail[A](s: Syntax[A]): Factorization[A] = {
-        Factorization(
-          failure,
-          s
-        )
-      }
-    }
-
-    def leftFactorOut[A](s: Syntax[A]): Factorization[A] = {
-      s match {
-        case _ if s == leftFactor       => Factorization.success.asInstanceOf[Factorization[A]]
-        case e: Elem                    => Factorization.fail(e)
-        case Sequence(l, r)             => {
-          val p = getProperties(l)
-          if(p.isNull){
-            leftFactorOut(r).prepend(l)
-          }
-          else{
-            // If the syntax is nullable, it might be possible
-            // that we should factorize both parts; we don't do this
-            // in order to stay simple (and because this case implies
-            // a first-follow conflict)
-            leftFactorOut(l) ~ r
-          }
-        }
-        case Disjunction(l, r)          => leftFactorOut(l) | leftFactorOut(r)
-        case Transform(fun, inv, inner) => leftFactorOut(inner).map(fun, inv)
-        case Marked(mark, inner)        => leftFactorOut(inner).mark(mark)
-        case s: Success[_]              => Factorization.fail(s)
-        case Failure()                  => Factorization(failure, failure)
-        case Recursive(_, inner)        => leftFactorOut(inner).asRecursive
-      }
-    }
-
-    leftFactorOut(s).complete
+    internal(leftFactor, s).complete(leftFactor)
   }
+
+  /** Left factorizes a single terminal in the syntax.
+  * 
+  * @param leftFactor Terminal to left factor out.
+  * @param s Syntax on which to apply the factorization.
+  * @return An equivalent syntax, with the terminal factorized.
+  *
+  * @group factorization
+  */
+  def leftFactorize[A](leftFactor: Kind, s: Syntax[A]): Syntax[A] = {
+    leftFactorize(Elem(leftFactor), s)
+  }
+  
 
 }
