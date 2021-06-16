bbf6622315b9691423ce195d8b43b962d2fb7baa
Noe De Santo <noe.desanto@epfl.ch>
2021-06-16 11:53:07 +0200

    Update conflict solving algorithm

--- src/main/scala/scallion/transformation/Transformation.scala
+++ src/main/scala/scallion/transformation/Transformation.scala
@@ -8,7 +8,7 @@ import scala.annotation.tailrec
  * 
  * @groupname conflicts Conflicts resolution
  */
-trait Transformations extends LeftFactorization with Substitution with Unfold with Split with LL1Properties {
+trait Transformations extends LeftFactorization with LeftRecursion with Substitution with Unfold with Split with LL1Properties {
   self: Syntaxes with Parsers => 
 
   import Conflict._
@@ -20,27 +20,43 @@ trait Transformations extends LeftFactorization with Substitution with Unfold wi
     tokens.foldLeft(syntax)( (s, t) => eliminate(s, src, leftFactorizeKind(t, src)))
   }
 
+  private def solveFollowConflict[A](syntax: Syntax[A], conflict: FollowConflict): Syntax[A] = {
+    val tokens = conflict.ambiguities
+    // The present cast doesn't have any meaning; it is just required in order to typecheck
+    val src = conflict.root.asInstanceOf[Syntax[Any]] 
+    tokens.foldLeft(syntax)( (s, t) => eliminate(s, src, leftFactorizeKind(t, src)))
+  }
+
   /**
-    * Attempt to solve first-first conflicts inside the syntax
+    * Attempt to solve conflicts inside the syntax
     *
     * @param syntax The syntax with a conflict.
     * 
     * @group conflicts
     */
-  def solveFirstConflicts[A](syntax: Syntax[A]): Syntax[A] = {
+  def solveConflicts[A](syntax: Syntax[A]): Syntax[A] = {
 
     @tailrec
-    def iter[B](syntax: Syntax[B], conflicts: List[Conflict]): Syntax[B] = {
+    def iter(syntax: Syntax[A], conflicts: List[Conflict]): Syntax[A] = {
       conflicts match {
         case (fc: FirstConflict) :: _ => {
           val newSyntax = solveFirstConflict(syntax, fc)
-          iter(newSyntax, getConflicts(newSyntax).toList)
+          startIter(newSyntax)
+        }
+        case (fc: FollowConflict) :: _ => {
+          val newSyntax = solveFollowConflict(syntax, fc)
+          startIter(newSyntax)
         }
         case h :: tl => iter(syntax, tl)
         case Nil     => syntax
       }
     }
 
-    iter(syntax, getConflicts(syntax).toList)
+    def startIter(syntax: Syntax[A]): Syntax[A] = {
+      val s = eliminateLeftRecursions(syntax)
+      iter(s, getConflicts(s).toList)
+    }
+
+    startIter(syntax)
   }
 }
---
+++ src/test/scala/scallion/transformation/TransformationsTests.scala
@@ -0,0 +1,56 @@
+package scallion
+package transformation
+
+import org.scalatest._
+
+class TransformationsTests extends ParsersTestHelper with Transformations with BooleanSyntaxes {
+
+  "Solve conflicts" should "work on simple first/first example" in {
+    val grammar = (tru ~ falz) | (tru ~ tru)
+    val solved = solveConflicts(grammar)
+
+    assertHasConflicts(grammar)
+    val parser = assertIsLL1(solved)
+
+    assertParses(getValue(_), valuePrinter(_))(parser, grammar, ParsersTestHelper.MediumSample)
+  }
+
+  it should "work on simple first/follow example" in {
+    val grammar = (tru | epsF)  ~ tru
+    val solved = solveConflicts(grammar)
+
+    assertHasConflicts(grammar)
+    val parser = assertIsLL1(solved)
+
+    assertParses(getValue(_), valuePrinter(_))(parser, grammar, ParsersTestHelper.MediumSample)
+  }
+
+  it should "work on simple left recursive example" in {
+    lazy val grammar: Syntax[Boolean] = recursive {
+      (grammar ~ tru).map(orComb) | epsT
+    }
+    val solved = solveConflicts(grammar)
+
+    assertHasConflicts(grammar)
+    val parser = assertIsLL1(solved)
+
+    assertParses(getValue(_), valuePrinter(_))(parser, grammar, ParsersTestHelper.MediumSample)
+  }
+
+  it should "work on `not immediately left recursise` syntaxes" in {
+    lazy val rec: Syntax[Boolean] = recursive {
+      (tru ~ rec).map(orComb) | epsT
+    }
+    val grammar = rec | (tru ~ falz).map(orComb)
+    val solved = solveConflicts(grammar)
+
+    assertHasConflicts(grammar)
+    val parser = assertIsLL1(solved)
+
+    assertParseResult(true)(parser, List(true, false))
+    assertParseResult(true)(parser, List(true))
+    assertParseResult(true)(parser, List(true, true, true))
+    assertParses(getValue(_), valuePrinter(_))(parser, grammar, ParsersTestHelper.MediumSample)
+  }
+
+}
